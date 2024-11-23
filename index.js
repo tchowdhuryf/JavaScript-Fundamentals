54232c5491dd3fdd8b3be7c695c9646c8b38e44b
tchowdhuryf <tasnim.chowdhury837@gmail.com>
2024-11-22 20:49:29 -0500

    validation,try/catch,calculate score

--- index.js
+++ index.js
@@ -88,35 +88,61 @@ const LearnerSubmissions = [
 // with more points_possible should be counted for more
 // e.g. a learner with 50/100 on one assignment and 190/200 on another
 // would have a weighted average score of 240/300 = 80%.
-//
+
 // each assignment should have a key with its ID,
 // and the value associated with it should be the percentage that
 // the learner scored on the assignment (submission.score / points_possible)
-//
+
 // if an assignment is not yet due, it should not be included in either
 // the average or the keyed dictionary of scores
   
+// 
 function getLearnerData(course, ag, submissions) {
-    // here, we would process this data to achieve the desired result.
-    const result = [
-      {
-        // the ID of the learner for which this data has been collected
-        id: 125,
-        avg: 0.985, // (47 + 150) / (50 + 150)
-        1: 0.94, // 47 / 50
-        2: 1.0 // 150 / 150
-      },
-      {
-        id: 132,
-        avg: 0.82, // (39 + 125) / (50 + 150)
-        1: 0.78, // 39 / 50
-        2: 0.833 // late: (140 - 15) / 150
+  // Checking if course id match
+  if (course.id !== ag.course_id) {
+    throw new Error("Course ID does not match the assignment group");
+  }
+
+  // Calculating the score for an assignment
+  function calculateScore(submission, assignment) {
+    // Checking to make sure that the points possible for assignment is not zero
+    try {
+      if (assignment.points_possible === 0) {
+        throw new Error(
+          `Assignment ${assignment.id} has zero points possible.`
+        );
       }
-    ];
+
+      let score = submission.score / assignment.points_possible;
+
+      // Checking if the submission is late and take 10% off if assignment is late
+      if (new Date(submission.submitted_at) > new Date(assignment.due_at)) {
+        score -= 0.1;
+      }
+
+      // Checking if the score is between 0 and 1. within 100%
+      return Math.max(0, Math.min(score, 1)); // Clamping to range [0, 1]
+
+    } catch (error) {
+      console.error(error); // If invalid score catch error return 0
+      return 0;
+    }
+  }
+
+  // Find the assignment
+  let assignment;
+  for (let i = 0; i < ag.assignments.length; i++) {
+    if (ag.assignments[i].id === assignment_id) {
+      assignment = ag.assignments[i];
+      break;
+    }
+  }
+
   
-    return result;
+
+  return result;
+  
 }
-  
 const result = getLearnerData(CourseInfo, AssignmentGroup, LearnerSubmissions);
-  
+
 console.log(result);
